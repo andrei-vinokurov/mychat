fa894ec257686cd71468c37c264921323a53e938
andrei-vinokurov <andrei-vinokurov@mail.ru>
2025-01-26 18:21:46 +0400

    2025 01 26

--- client/MyDialog.cpp
+++ client/MyDialog.cpp
@@ -2,10 +2,15 @@
 
 MyDialog::MyDialog(wxPanel* parent) : wxDialog(parent, wxID_ANY, wxT("Диалог"), wxDefaultPosition, wxSize(500, 600))
 {
-    m_text = new wxTextCtrl(this, wxID_ANY, "", wxPoint (0, 0), wxSize (500, 300), wxTE_MULTILINE | wxTE_READONLY);
+    m_text1 = new wxTextCtrl(this, wxID_ANY, "", wxPoint (0, 0), wxSize (500, 300), wxTE_MULTILINE | wxTE_READONLY);
+    m_text2 = new wxTextCtrl(this, wxID_ANY, "", wxPoint (0, 300), wxSize (500, 220), wxTE_MULTILINE);
+    m_send = new wxButton(this, wxID_ANY, wxT("Отправить"), wxPoint (200, 530), wxSize (100, 30));
+    m_send->Bind(wxEVT_BUTTON, &MyDialog::SendText, this);
+    
 }
 
-MyDialog::~MyDialog()
+void MyDialog::SendText(wxCommandEvent& event)
 {
-    delete m_text;
+    m_text1->AppendText(m_text2->GetValue());
+
 }
--- client/MyDialog.h
+++ client/MyDialog.h
@@ -5,15 +5,18 @@
 #include <wx/dialog.h>
 #include <wx/msgdlg.h>
 #include <wx/textdlg.h>
+#include <wx/button.h>
 
 class MyDialog : public wxDialog
 {
 public:
     MyDialog(wxPanel* parent);
-    ~MyDialog();
+    void SendText(wxCommandEvent& event);
 
 private:
-    wxTextCtrl* m_text;
+    wxTextCtrl* m_text1;
+    wxTextCtrl* m_text2;
+    wxButton* m_send;
 
 };
 
